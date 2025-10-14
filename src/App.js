3904741351e4fa92493c73137d0b42a4b4bfc67a
kruthika <k.b.chandrashekhar@accenture.com>
2025-10-14 12:18:38 +0530

    Added crud operations for express js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ const connectToDB = require('./config/database'); // Ensure database connection 
 const app = express();
 const User = require('./models/user');
 
+app.use(express.json());
+
 app.post('/signup', async (req, res) => {
-  const userObj = {
-    firstName: 'Kruthika',
-    email: 'kruthi@gmail.com',
-    password: 'Kruthi@123',
-    age: 22
-  }
-  const user = new User(userObj);
+  const user = new User(req.body);
   await user.save().then(() => {
     res.status(201).send('User created successfully');
   }).catch((error) => {
@@ -18,6 +14,55 @@ app.post('/signup', async (req, res) => {
   });
 });
 
+// Get user by email
+app.get('/users-by-email', async (req, res) => {
+  try {
+    const userEmail = await User.find({email: req.body.email});
+    if (userEmail.length === 0) {
+      return res.status(404).send('No users found with the specified email');
+    }
+    else { 
+      return res.status(200).json(userEmail);
+    }
+  } catch (error) {
+   res.status(500).json("Server error: " + error.message);
+  }
+});
+
+// Get all users
+app.get('/users-all', async (req, res) => {
+  try {
+    const userEmail = await User.find({});
+    if (userEmail.length === 0) {
+      return res.status(404).send('No users found with the specified email');
+    }
+    else { 
+      return res.status(200).json(userEmail);
+    }
+  } catch (error) {
+   res.status(500).json("Server error: " + error.message);
+  }
+});
+
+// Delete user by ID
+app.delete('/users-delete/:id', async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    return res.send("Deleted successfully");
+  } catch (error) {
+   res.status(500).json("Server error: " + error.message);
+  }
+});
+
+// update user by ID
+app.put('/users-update/:id', async (req, res) => {
+  try {
+    await User.findByIdAndUpdate(req.params.id, req.body)
+    return res.status(200).send("Updated successfully");
+  } catch (error) {
+   res.status(500).json("Server error: " + error.message);
+  }
+});
 
 connectToDB()
   .then(() => {
